75f7a738b5a2a17320c9f51f473195d3e00baafe
Mac Book Air M1 <macbookairm1@Macs-MacBook-Air-2.local>
2026-02-21 23:08:05 +0700

    Add environment control

--- src/config/connect.config.ts
+++ src/config/connect.config.ts
@@ -1,20 +1,34 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const connectionConfig = {
-    host: '175.100.42.58',
-    user: 'ltr-userdb',
-    password: 'I7p7?)3zF-vx',
-    database: 'ecommerce_db',
-    port: 3306
+    development: {
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'ltr_supply_db',
+        port: 8889
+    },
+    production: {
+        host: '175.100.42.58',
+        user: 'ltr-userdb',
+        password: 'I7p7?)3zF-vx',
+        database: 'ecommerce_db',
+        port: 3306
+    }
 };
 
-const directConnection = mysql.createConnection(connectionConfig);
+const dataConfig = process.env.NODE_ENV === 'production' ? connectionConfig.production : connectionConfig.development;
+
+const directConnection = mysql.createConnection(dataConfig);
 directConnection.connect(err => {
     if (err) {
         console.error('Error connecting: ' + err);
         return;
     }
-    console.log('Direct connection successful on port %s as id %d', connectionConfig.port, directConnection.threadId);
+    console.log('Direct connection successful on port %s as id %d', dataConfig.port, directConnection.threadId);
 });
 
 export default directConnection

b4dc18f5488059d686d46210d03a2501119aebbf
Mac Book Air M1 <macbookairm1@Macs-MacBook-Air-2.local>
2026-02-21 16:42:04 +0700

    Fix conflict cors

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const app:Application = express();
 
 // CORS Configuration
 const corsOptions = {
-    origin: ['*', 'http://localhost:5173', 'http://localhost:4173', 'ltrcambodia.com'],
+    origin: ['*', 'http://localhost:5173', 'http://localhost:4173', 'http://ltrcambodia.com'],
     credentials: true,
     methods: 'GET, POST, PUT, DELETE',
     optionsSuccessStatus: 200,

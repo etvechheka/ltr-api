aeb4cb128535117ff1d8d8ce5e32fc5e94524eab
Mac Book Air M1 <macbookairm1@Macs-MacBook-Air-2.local>
2026-02-21 16:18:43 +0700

    Update Service

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const app:Application = express();
 
 // CORS Configuration
 const corsOptions = {
-    origin: ['*', 'http://localhost:5173', 'http://localhost:4173'],
+    origin: ['*', 'http://localhost:5173', 'http://localhost:4173', 'ltrcambodia.com'],
     credentials: true,
     methods: 'GET, POST, PUT, DELETE',
     optionsSuccessStatus: 200,

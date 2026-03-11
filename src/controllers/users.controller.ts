38341f87e0cc3e0952ab04c4fde6a825e0013cc2
Mac Book Air M1 <macbookairm1@Macs-MacBook-Air-2.local>
2026-03-11 21:40:18 +0700

    update user

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -3,87 +3,81 @@ import { getAllUsers, getUserById, updateUserById } from "../models/user.model";
 import { User } from "../schema/userValid.schema";
 
 
-export const getUsers = (req: Request, res: Response, next: NextFunction) => {
+export const getUsers = async (req: Request, res: Response, next: NextFunction) => {
 
     try {
-        getAllUsers((err, result) => {
-            if (err) throw err;
-            if (result) {
-                return res.status(200).json({
-                    status: true,
-                    message: 'Data has been fetch successfully',
-                    result: result
-                })
-            }
-            res.status(404).json({
-                status: false,
-                message: 'Data do not found'
+        const result: any = await getAllUsers();
+        if (result.length !== 0) {
+            return res.status(200).json({
+                status: true,
+                message: 'Data has been fetch successfully',
+                result: result
             })
-        });
+        }
+
+        res.status(404).json({
+            status: false,
+            message: 'Data do not found'
+        })
     } catch (error) {
         next(error)
     }
 }
 
-export const getUser = (req: Request, res: Response, next: NextFunction) => {
+export const getUser = async (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params
 
     try {
-        getUserById(id as string, (err, result) => {
-            if (err) throw err;
-            if (result) {
-                return res.status(200).json({
-                    status: true,
-                    message: 'Data fetch successfully',
-                    result: result[0]
-                })
-            }
-
-            res.status(404).json({
-                status: false,
-                message: 'Data do not found'
+        const result: any = await getUserById(id as string);
+        if (result.length !== 0) {
+            return res.status(200).json({
+                status: true,
+                message: 'Data fetch successfully',
+                result: result[0]
             })
+        }
+
+        res.status(404).json({
+            status: false,
+            message: 'Data do not found'
         })
     } catch (error) {
         next(error);
     }
 }
 
-export const updateUser = (req: Request, res: Response, next: NextFunction) => {
+export const updateUser = async (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params;
     const {
         full_name, email, username, password, role, date_of_birth, status
     }: User = req.body;
 
     try {
-        getUserById(id as string, (err, result) => {
-            if (err) throw err;
-            if (result.length == 0) {
-                return res.status(404).json({
-                    status: false,
-                    message: 'Could not update this user'
-                })
-            }
-            
-            const newData = {
-                full_name: full_name,
-                username: username,
-                email: email,
-                date_of_birth: date_of_birth,
-                password_hash: password,
-                role: role,
-                status: status
-            }
+        const result: any = await getUserById(id as string);
 
-            updateUserById(id as string, newData, (err, result) => {
-                if(err) throw err;
-                res.status(201).json({
-                    status: true,
-                    message: 'User has been updated',
-                    result: newData
-                })
-            });
+        if (result.length == 0) {
+            return res.status(404).json({
+                status: false,
+                message: 'Could not update this user'
+            })
+        }
+
+        const newData = {
+            full_name: full_name,
+            username: username,
+            email: email,
+            date_of_birth: date_of_birth,
+            password_hash: password,
+            role: role,
+            status: status
+        }
+        updateUserById(id as string, newData);
+        res.status(201).json({
+            status: true,
+            message: 'User has been updated',
+            result: newData
         })
+
     } catch (error) {
         next(error)
     }
